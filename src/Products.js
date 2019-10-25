557ce05a269e4edc2a6b1acaa2f6516e77b4a6a9
renenjovalente-ubidy <renenjo.valente@ubidy.com>
2019-10-25 18:52:20 +0800

    Changes: initial

---
+++ src/Products.js
@@ -0,0 +1,74 @@
+export const productsFirstRow = [
+  {
+    title: 'Spanjaard Cold Zinc Galvanising Spray',
+    description:
+      'Used as a protective final coating or primer wherever iron or steel will be exposed to corrosive conditions.  The spray-film contains very fine particles of 95% pure zinc and aluminum.  The film dries to a flexible, noncracking, non-chipping coating.',
+    imgUrl: './images/spanjaard-cold-zinc-galvanising-spray.jpg',
+  },
+  {
+    title: 'Spanjaard Metal Crack Detector',
+    description:
+      'System consists of 3 products for the convenient nondestructive testing (NDT) of cracks and other surface flaws in metal components.  The system is a fast, economical and reliable method of visual detection of all surface defects on metal parts during manufacture and also on operational components in-situ.  The three pack system meets DTD 929/MIL-I-25135E Group I and II.',
+    imgUrl: './images/spanjaard-metal-crack-detector.jpg',
+  },
+  {
+    title: 'Spanjaard Electrical Insulating Varnish',
+    description:
+      'A red coloured, moisture and oil resistant varnish. It is an excellent insulating medium for use in a variety of industrial applications. Protects against arcing from controller shafts, switch bases and other electrical equipment',
+    imgUrl: './images/spanjaard-electrical-insulating-varnish.jpg',
+  },
+  {
+    title: 'Super Lube® Food Grade',
+    description:
+      'Anti-Seize Lubricant with Syncolon® (PTFE) is an NSF registered Food Grade lubricant, rated H1 for incidental food contact and meets former USDA (H1) guidelines',
+    imgUrl: './images/super-lube.jpg',
+  },
+];
+
+export const productsSecondRow = [
+  {
+    title: 'American Grease HIGH TEMPERATURE GREASE',
+    description:
+      'Engineered to meet the demands of protecting high-speed bearings in applications involving high temperatures and heavy mechanical loads. (1) High temperature lubrication (drop point 625°F)',
+    imgUrl: './images/high-temperature-grease.jpg',
+  },
+  {
+    title: 'Hydraulic Oils',
+    description:
+      'Are suitable for most hydraulic systems where high levels of anti-wear and protection are required in conjunction with good service life reliability. They are also suitable for a wide range of other applications where machinery lubricants with excellent anti-wear lubrication and resistance to degradation in service are required. Applications can include systems incorporating high speed gears and bearings, pumps, blowers and other uses where this type and quality of lubricant can assist with lubricants rationalisation for the ISO viscosity grades available. They are formulated from solvent-refined base oils and incorporate additive treatments to provide excellent service life with protection against corrosion, wear, oxidation and foaming',
+    imgUrl: './images/hydraulic-oils.jpg',
+  },
+  {
+    title: 'Pnuematic oil / Air Tool Oil',
+    description:
+      'Specifically designed and formulated for use with all piston and rotary type air tools. The special sulphurised fatty ester additives give a lubricant with minimum sulphur corrosivity together with maximum lubricating value and load carrying capacity. It is eminently suitable for both ferrous and non-ferrous metals. It lubricates, cleans and protects the air tool to which it is applied. Tool life may be dramatically extended by the removal of gum deposits by Rock Air Tool Oil, and improved power and speed is also provided',
+    imgUrl: './images/pnuematic-oil.jpg',
+  },
+  {
+    title: 'Compressor Oils',
+    description:
+      'Blended using high vi premium grade base oils and selected additives to provide long-term protection and reliability for your compressor. The new generation formula using compressor specific chemistry enables the AC 2000 range to be used in all types of compressor',
+    imgUrl: './images/compressor-oils.jpg',
+  },
+];
+
+export const productsThirdRow = [
+  {
+    title: 'Electrical Contact Lubricant',
+    description:
+      'Electrical Contact Lubricant 1010 is a special product for lubricating electrical contacts. Spanjaard Electrical Contact Lubricant 1010 is specially formulated to give low carbon forming tendencies and to lubricate and alleviate these problems and thus prolong contact life',
+    imgUrl: './images/spanjaard.jpg',
+  },
+  {
+    title: 'PU-Foam',
+    description:
+      'A 1-component insulation and construction polyurethane foam developed to create a seal between construction parts.  PU-Foam cures in response to moisture from the air and the building material',
+    imgUrl: './images/pu-foam.jpg',
+  },
+  {
+    title: 'Cold Zinc Galvanising Paint',
+    description:
+      'A fast drying anti-corrosive primer containing zinc dust and aluminium. Suitable for use on non-ferrous metals and has been developed for touching up the edges of cut galvanized sheet metal. Suitable for direct application as supplied by brush or spray. Can be diluted with acetone for use as an aerosol intermediate.',
+    imgUrl: './images/cold-zinc-galvanising-paint.jpg',
+  },
+];
